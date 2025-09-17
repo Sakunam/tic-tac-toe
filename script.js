b4208e5902ce974ff8824d11cbab646daadab82e
Michael Han <mhan242424@gmail.com>
2025-09-16 21:16:19 -0400

    Fix Game Winning Criteria
    gameboard.someoneWon now checks that the character
    of the tile is not the default/empty tile
    character.

--- script.js
+++ script.js
@@ -11,14 +11,22 @@ const gameboard = (function() {
     }
 
     const someoneWon = () => {
-        if ((board[0][0] == board[1][0] && board[1][0] == board[2][0]) ||
-            (board[0][1] == board[1][1] && board[1][1] == board[2][1]) ||
-            (board[0][2] == board[1][2] && board[1][2] == board[2][2]) ||
-            (board[0][0] == board[0][1] && board[0][1] == board[0][2]) ||
-            (board[1][0] == board[1][1] && board[1][1] == board[1][2]) ||
-            (board[2][0] == board[2][1] && board[2][1] == board[2][2]) ||
-            (board[0][0] == board[1][1] && board[1][1] == board[2][2]) ||
-            (board[2][0] == board[1][1] && board[1][1] == board[0][2])) {
+        if ((board[0][0] != defaultChar && 
+             board[0][0] == board[1][0] && board[1][0] == board[2][0]) ||
+            (board[0][1] != defaultChar && 
+             board[0][1] == board[1][1] && board[1][1] == board[2][1]) ||
+            (board[0][2] != defaultChar && 
+             board[0][2] == board[1][2] && board[1][2] == board[2][2]) ||
+            (board[0][0] != defaultChar && 
+             board[0][0] == board[0][1] && board[0][1] == board[0][2]) ||
+            (board[1][0] != defaultChar && 
+             board[1][0] == board[1][1] && board[1][1] == board[1][2]) ||
+            (board[2][0] != defaultChar &&
+             board[2][0] == board[2][1] && board[2][1] == board[2][2]) ||
+            (board[0][0] != defaultChar && 
+             board[0][0] == board[1][1] && board[1][1] == board[2][2]) ||
+            (board[2][0] != defaultChar && 
+             board[2][0] == board[1][1] && board[1][1] == board[0][2])) {
             return true;
         }
         else {
