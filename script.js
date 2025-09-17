89d57028aa098cae976cdc1621d1909af6c3ffed
Michael Han <mhan242424@gmail.com>
2025-09-16 20:52:35 -0400

    Check If Game Is Won
    Added a new simple function in gameboard that
    checks if the game has been won by somebody.

--- script.js
+++ script.js
@@ -6,6 +6,22 @@ const gameboard = (function() {
 
     const getBoard = () => board;
 
+    const someoneWon = () => {
+        if ((board[0][0] == board[1][0] && board[1][0] == board[2][0]) ||
+            (board[0][1] == board[1][1] && board[1][1] == board[2][1]) ||
+            (board[0][2] == board[1][2] && board[1][2] == board[2][2]) ||
+            (board[0][0] == board[0][1] && board[0][1] == board[0][2]) ||
+            (board[1][0] == board[1][1] && board[1][1] == board[1][2]) ||
+            (board[2][0] == board[2][1] && board[2][1] == board[2][2]) ||
+            (board[0][0] == board[1][1] && board[1][1] == board[2][2]) ||
+            (board[2][0] == board[1][1] && board[1][1] == board[0][2])) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
     const makeMove = (row, col, char) => {
         if (board[row][col] == defaultChar) {
             board[row][col] = char;
@@ -17,6 +33,7 @@ const gameboard = (function() {
 
     return { 
         getBoard,
+        someoneWon,
         makeMove
     };
 })();
