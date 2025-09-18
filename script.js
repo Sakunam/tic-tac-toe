a2a5212dafc1aaf31263f755d94ad6f55702cc1f
Michael Han <mhan242424@gmail.com>
2025-09-18 15:22:03 -0400

    Remove Core Gameplay Loop
    The gameManager.startGame method that controlled
    the game loop using a while loop has been reduced
    to an initialization method that simply sets the
    board and displays it using methods from other
    objects.

--- script.js
+++ script.js
@@ -64,28 +64,7 @@ function createPlayer(name) {
 const gameManager = (function() {
     const startGame = () => {
         gameboard.setBoard();
-        let validMoves = 0;
-        while (validMoves < 9 && !gameboard.someoneWon()) {
-            const rowMove = prompt("row move?");
-            const colMove = prompt("col move?");
-            
-            if (validMoves % 2 == 0) {
-                if (gameboard.makeMove(rowMove, colMove, 'X')) {
-                    validMoves++;
-                }
-            }
-            else {
-                if (gameboard.makeMove(rowMove, colMove, 'O')) {
-                    validMoves++;
-                }
-            }
-
-            alert(`${gameboard.getBoard()[0]}\n${gameboard.getBoard()[1]}\n${gameboard.getBoard()[2]}`)
-        }
-
-        if (!gameboard.someoneWon()) {
-            alert("It's a tie!");
-        }
+        displayController.displayBoard();
     }
 
     return {
